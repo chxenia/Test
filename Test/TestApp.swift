0428a61f95207eca9a8e31db1f02f78fb3b536ea
Ksenia Chernyshova <95574685+xenia155@users.noreply.github.com>
2024-10-04 13:11:27 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ksenya Chernyshova on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
